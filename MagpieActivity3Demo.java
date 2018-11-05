5a964a959a3b4a36fef01b631395b3f068c1f5ee
AHS-MHaugh <43642385+AHS-MHaugh@users.noreply.github.com>
2018-11-04 23:35:08 -0500

    Add files via upload

---
+++ MagpieActivity3Demo.java
@@ -0,0 +1,42 @@
+/**
+ *Michael Haugh
+ *Magpie Activity 3
+ *AP CS
+ *11-3-18
+ */
+
+import java.util.Scanner;
+
+/**
+ * A simple class to run the Magpie class.
+ * @author Laurie White
+ * @version April 2012
+ */
+public class MagpieActivity3Demo{
+    //^This is the class.^
+
+	/**
+	 * Create a Magpie, give it user input, and print its replies.
+	 */
+	public static void main(String[] args)
+	{
+		MagpieActivity3 maggie = new MagpieActivity3();
+            // ^This is the object.^
+		
+		System.out.println (maggie.getGreeting());
+            //^This is the greeting print command.^
+		Scanner in = new Scanner (System.in);
+            //^This is the scanner object.^
+		String statement = in.nextLine();
+            //^nextLine method adds a new line after the response is received.^
+		
+		while (!statement.equals("Bye")){
+            //^Allows the response to be sent if the input is not "Bye".^
+			System.out.println (maggie.getResponse(statement));
+            //^This is the print command.^
+			statement = in.nextLine();
+            //^Adds a new line after response is received.^
+		}
+	}
+
+}
